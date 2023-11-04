e9ec7a072d9831fb31f1dc381076b87a2c9330d3
afrrn <149873112+afrrn@users.noreply.github.com>
2023-11-04 21:30:47 +0000

    datatypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,26 @@
+"use strict "; // treat all JS code as newer version
+// alert(3+3);  shows error as alert not found we are using node js , not browser . 
+// data type
+let Name = "Afreen"
+let age = 23;
+let isLoggedIn=true;
+
+/* 1. number
+   2. bigint
+   3. string
+   4. boolean
+   5. null => stanalone value 
+   6. undefined => value is still not assigned
+   7. symbol => unique 
+   8. object
+*/
+console.log(typeof age );
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof Name);
+console.log(typeof isLoggedIn);
+
+
+  
+
+            
